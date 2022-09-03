1c50bdec2809c1fd18c9ae30f6579d90eb9302fc
Md-TarikulIslam <nayon1820@gmail.com>
2022-09-03 22:12:12 +0600

    final commit

--- app.js
+++ app.js
@@ -1,43 +1,32 @@
 const loadCategories = async () => {
+    try {
+        const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+    }
 
-    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
-    const data = await response.json();
-    return data;
 }
 const setMenu = async () => {
     const data = await loadCategories();
-
-
-    // console.log(data.data.news_category)
-    // console.log(data.data)
     const objects = data.data.news_category
-    // objects.forEach(obj => console.log(obj))
-    // const lists = data.data.news_category
     const menus = document.getElementById('menus');
     for (const list of objects) {
-        // console.log(list.category_name)
         const li = document.createElement('li');
-        // li.classList.add('menu')
         li.innerHTML = `
         <a onclick="loadId(${list.category_id})">${list.category_name}</a>
-        
         `;
         menus.appendChild(li);
-
     }
 }
 
 const loadId = async (category_id) => {
-
-
     const url = `https://openapi.programming-hero.com/api/news/category/0${category_id}`
-    // console.log(url)
     const response = await fetch(url);
     const data = await response.json();
-
     displayNews(data.data);
-
-
 }
 const toggle = isLoading => {
     const loaderSection = document.getElementById('loader');
@@ -50,12 +39,12 @@ const toggle = isLoading => {
 }
 const displayNews = async news => {
     const data = await loadCategories();
-    // console.log(news);
+    news.sort((a, b) => b.total_view - a.total_view);
+    console.log(news);
     const number = document.getElementById('number')
     number.innerHTML = `
-    <p>${news.length} items founded in portal news {} </p>
+    <p>${news.length} items founded in portal news</p>
     `
-    // console.log(number)
     toggle(true)
     const details = document.getElementById('details');
 
@@ -69,11 +58,8 @@ const displayNews = async news => {
         newsDiv.classList.add(`shadow-xl`);
         newsDiv.classList.add(`m-8`);
         newsDiv.classList.add(`p-8`);
-
         newsDiv.innerHTML = `
-   
     <figure><img  src="${article.thumbnail_url}" alt="Album"></figure>
-
     <div class="card-body">
     <h2 class="card-title">${article.title}</h2>
     <p>${article.details.length > 300 ? article.details.slice(0, 300) + '...' : article.details}</p>
@@ -81,29 +67,19 @@ const displayNews = async news => {
    <img class="w-10 rounded-full" src="${article.author.img}">
     <p class="ml-4 mt-2"> ${article.author.name ? article.author.name : 'No Author Name Found'}</p>
     <p><i class="fa-regular fa-calendar"></i> ${article.author.published_date}</p>
-    
     <p><i class="fa-regular fa-eye"></i> ${article.total_view ? article.total_view : 'No Views Found'}</p>
-    
     </div>
     <div class="card-actions justify-end">
-    <label for="my-modal-4" onclick="loadPop('${article._id}')" class="btn modal-button">Listen</label>
+    <label for="my-modal-4" onclick="loadPop('${article._id}')" class="btn modal-button">Details</label>
     </div>
   </div>
-
-        
         `;
         details.appendChild(newsDiv);
-
     })
     toggle(false)
-
-
-
 }
-
-
+loadId(08);
 const loadPop = (id) => {
-    // console.log(id)
     fetch(`https://openapi.programming-hero.com/api/news/${id}`)
         .then(res => res.json())
         .then(data => showdetailsModal(data.data[0]))
@@ -118,19 +94,10 @@ const showdetailsModal = (modal) => {
    <p>${details}</p>
    <p class="py-4"><i class="fa-solid fa-pen-nib"></i> ${author.name ? author.name : 'No data available'}</p>
    <p class="py-4"><i class="fa-regular fa-eye"></i> ${total_view ? total_view : 'No data available'}</p>
-   
-   
-   `
-
+   `;
 }
 
+loadId();
+setMenu();
 
-
-loadId()
-
-
-// displayV()
-
-setMenu()
-// loadCategories()
 
